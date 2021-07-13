9c51e1194c9fecc8fd407adc914a475ab9a950e3
Stoyan Ivanov <SSIvanov19@codingburgas.bg>
2021-07-13 14:27:54 +0300

    Implement link extracting tool

--- adpProject/adpProject/items.py
+++ adpProject/adpProject/items.py
@@ -6,7 +6,9 @@
 import scrapy
 
 
-class AdpprojectItem(scrapy.Item):
+class FandomItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    name = scrapy.Field();
+    country = scrapy.Field();
+    imgLink = scrapy.Field();
--- adpProject/adpProject/spiders/fandom.py
+++ adpProject/adpProject/spiders/fandom.py
@@ -1,10 +1,21 @@
 import scrapy
-
+from adpProject.items import FandomItem
 
 class FandomSpider(scrapy.Spider):
     name = 'fandom'
-    allowed_domains = ['https://dota2.fandom.com/']
+    allowed_domains = ['dota2.fandom.com']
     start_urls = ['https://dota2.fandom.com/wiki/Professional_teams']
 
     def parse(self, response):
-        item = 
+        ourList = [] #komunizam
+        item = FandomItem()
+        for href in response.css("div.teambox-name > a::attr(href)").extract():
+            ourList.append(href)
+        
+        print()
+        print()
+        print('Tuka gledaj')
+        print(ourList)
+        print()
+        print()
+        
